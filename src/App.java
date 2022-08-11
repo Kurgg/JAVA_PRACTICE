2ce6dc9b3afe68cf8e2abdf525c2fb52ef02312e
Kurgg <markus.kurg@gmail.com>
2022-08-11 21:57:58 +0300

    new file:   .vscode/settings.json new file:   README.md new file:   bin/App.class new file:   src/App.java

---
+++ src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hope this code works well!");
+        System.out.println("Here is an calculation");
+        System.out.println(3 + 3 - 2);
+        System.out.print("This is how to do differently. ");
+        System.out.print("I will print on the same line.");
+        // I have commented this out with double slashes
+      }
+}
