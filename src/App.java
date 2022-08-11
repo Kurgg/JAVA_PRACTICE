c309480392d361f2a53a441b73806f34160a3bb9
Kurgg <markus.kurg@gmail.com>
2022-08-11 22:20:05 +0300

    modified:   src/App.java

--- src/App.java
+++ src/App.java
@@ -5,7 +5,31 @@ public class App {
         System.out.println("Here is an calculation");
         System.out.println(3 + 3 - 2);
         System.out.print("This is how to do differently. ");
-        System.out.print("I will print on the same line.");
+        System.out.println("I will print on the same line.");
         // I have commented this out with double slashes
-      }
+        /*This is a multiline comment, you can enter as much as you need to */
+        //--String contains text--
+        String name = "John";
+        System.out.println(name);
+        //--int only numbers--
+        int number = 15;
+        System.out.println(number);
+        //The number has been now changed
+        number = 20;
+        System.out.println(number);
+        //Now I'm going to set the final value to it so it can't be changed
+        final int number2 = 10;
+        System.out.println(number2);
+        //--float point numbers--
+        float myfloat = 5.99f;
+        System.out.println(myfloat);
+        //--char for single characters
+        char letter = 'D';
+        System.out.println(letter);
+        //--boolean for true or false
+        boolean bool = true;
+        System.out.println(bool);
+
+    }   
 }
+
