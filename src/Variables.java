eb106e2e81df8940a457ed972a5f9eb8bc11a664
Kurgg <markus.kurg@gmail.com>
2022-08-11 22:53:35 +0300

    modified:   bin/Variables.class modified:   src/Variables.java

--- src/Variables.java
+++ src/Variables.java
@@ -8,9 +8,25 @@ public class Variables {
             String fullname = first + name;
             System.out.println(fullname);
             //And now with math as well
-            int x = 5;
-            int y = 12;
+            int x = 5, y = 12;
+            //Set everything to the same value
+            x = y = 20;
             System.out.println(x + y);
-            
+            //"It is reccommended to use descriptive names for variables" - W3
+            byte bit = 100;
+            System.out.println(bit);
+            //--Floating Point Typess
+            float num1 = 57e5f; //e Indicates the power of 10, aka 10 times 5
+            double num2 = 12.87d;
+            System.out.println(num1);
+            System.out.println(num2);
+            //With "char" values, you can also use ASCII values
+            //--IF STUFF
+            //&& - true if both are true
+            //|| If one is true
+            //! if false then true (!(x < 5 && x < 10))
+            //-- STRING LENGTH
+            String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            System.out.println("The length of the text is: " + txt.length());
     }
 }
