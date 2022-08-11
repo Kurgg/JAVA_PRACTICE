b82e12cb1fa82b52290220f1691d51108da72d28
Kurgg <markus.kurg@gmail.com>
2022-08-11 23:12:37 +0300

    modified:   src/Variables.java

--- src/Variables.java
+++ src/Variables.java
@@ -28,5 +28,22 @@ public class Variables {
             //-- STRING LENGTH
             String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
             System.out.println("The length of the text is: " + txt.length());
-    }
+            //Setting letters to upper and lower cases
+            System.out.println(txt.toUpperCase());
+            System.out.println(txt.toLowerCase());
+            System.out.println(txt.indexOf("C"));
+            String txt2 = "I am a \"Warrior\" from the west"; // \" for double \' for single
+            System.out.println(txt2);
+            //-- JAVA MATH
+            Math.max(5,100);//Finds the biggest number inbetween the 2 given valuables
+            Math.min(5,100);
+            Math.sqrt(10);//Finds the square root of the said number
+            Math.abs(-4.7);//turns negative into positive
+            Math.random();//Random number between 0 and 1
+            int randomNum = (int)(Math.random()* 101);//Random number inbetween 0 and 100
+            //-- JAVA BOOTLEANS
+            int x2 = 10;
+            System.out.println(x2 == 10); //Should bring the results in being true
+            
+        }
 }
