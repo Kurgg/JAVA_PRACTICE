36b55baf2c9406d524cf7ec0b684b43fde7542dd
Kurgg <markus.kurg@gmail.com>
2022-08-11 22:28:29 +0300

    modified:   bin/Variables.class modified:   src/Variables.java

--- src/Variables.java
+++ src/Variables.java
@@ -1,5 +1,16 @@
 public class Variables {
         public static void main(String[] args) {
+            //Here I took a look on variables and the advanced use of it
+            String name = "Kurg";
+            System.out.println("Hello "+ name);
+            //And now a quick way to add family names
+            String first = "Mark ";
+            String fullname = first + name;
+            System.out.println(fullname);
+            //And now with math as well
+            int x = 5;
+            int y = 12;
+            System.out.println(x + y);
             
     }
 }
